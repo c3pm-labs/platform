0dea510d1ace1bdd4263ccf49ca53759b8dda902
Gabriel Colson <gabrielcolson@hotmail.fr>
2020-12-09 15:53:24 +0100

    fix(front): graphql requests

--- front/queries/index.ts
+++ front/queries/index.ts
@@ -7,12 +7,12 @@ export const USER = gql`
             username,
             email,
             description,
-            packages(first: 0, last: 1000) {
+            packages(first: 1000, last: 0) {
                 name
                 author {
                     username
                 }
-                versions(first: 0, last: 1000) {
+                versions(first: 1000, last: 0) {
                     version
                     publishedAt
                 }
@@ -78,7 +78,7 @@ export const PACKAGE_FROM_VERSION = gql`
                     username
                     id
                 }
-                versions(first: 0, last: 1000) {
+                versions(first: 1000, last: 0) {
                     version
                     publishedAt
                 }
@@ -95,7 +95,7 @@ export const SEARCH = gql`
     query search($keyword: String!) {
         search(keyword: $keyword) {
             name,
-            versions(first: 0, last: 1000) {
+            versions(first: 1000, last: 0) {
                 description,
                 license,
                 readme,
