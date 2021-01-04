803d2ed2c2a0bbc3349a26bee5f09c3d375b5553
ItagibaALIX <itagiba@redpill.paris>
2021-01-04 12:06:03 +0100

    feat: saving draft for context test

--- front/components/Layout/__tests__/LayoutToTest.tsx
+++ front/components/Layout/__tests__/LayoutToTest.tsx
@@ -1,15 +1,41 @@
-// import { render } from "@testing-library/react";
-// import { getDataFromTree } from '@apollo/react-ssr';
-// import withApollo from 'utils/withApollo';
+import { getDataFromTree } from '@apollo/react-ssr';
 
-// import Layout from "../index";
+import withApollo from 'utils/withApollo';
 
-// function LayoutToTest(): JSX.Element {
-//   return (
-//     <Layout>
-//       Toto
-//     </Layout>
+import Layout from '../index';
+
+function LayoutToTest(): JSX.Element {
+  return (
+    <Layout>
+      Toto
+    </Layout>
+  );
+}
+
+export default withApollo(LayoutToTest, { getDataFromTree });
+
+
+
+
+// import { render, cleanup, waitFor } from '@testing-library/react';
+// import { createRouter } from 'next/router';
+// import { RouterContext } from 'next-server/dist/lib/router-context';
+
+// const router = createRouter('', { user: 'nikita' }, '', {
+//   initialProps: {},
+//   pageLoader: jest.fn(),
+//   App: jest.fn(),
+//   Component: jest.fn(),
+// });
+
+
+// afterEach(cleanup);
+
+// it('Should render correctly on route: /users/nikita', async () => {
+//   const { getByText } = render(
+//     <RouterContext.Provider value={router}>
+//     </RouterContext.Provider>,
 //   );
-// }
 
-// export default withApollo(LayoutToTest, { getDataFromTree });
+//   await waitFor(() => getByText(/Hello nikita!/i));
+// });
--- front/components/Layout/__tests__/layout.tsx
+++ front/components/Layout/__tests__/layout.tsx
@@ -1,19 +1,93 @@
-// import { render } from "@testing-library/react";
-// import LayoutToTest from "./layoutToTest";
+import { render, cleanup, waitFor } from '@testing-library/react';
+import { createRouter, useRouter } from 'next/router';
+import { RouterContext } from 'next-server/dist/lib/router-context';
+// import Router from 'next/router';
 
-// test("Layout", () => {
-//   const { container } = render(<LayoutToTest />);
-
-//   expect(container).toMatchInlineSnapshot(``);
+// const router = createRouter('', { user: 'nikita' }, '', {
+//   initialProps: {},
+//   pageLoader: jest.fn(),
+//   App: jest.fn(),
+//   Component: jest.fn(),
 // });
 
+// import routeData from 'react-router';
+
+// const mockLocation = {
+//   pathname: '/welcome',
+//   hash: '',
+//   search: '',
+//   state: ''
+// }
+// beforeEach(() => {
+//   jest.spyOn(routeData, 'useLocation').mockReturnValue(mockLocation)
+// });
+
+// jest.mock('next/router', () => ({
+//   ...jest.requireActual('next/router'), // use actual for all non-hook parts
+//   useRouter: () => ({
+//     companyId: 'company-id1',
+//     teamId: 'team-id1',
+//   }),
+// }));
+
+import React from 'react';
+import { NextPage } from 'next';
+
+import LayoutToTest from './LayoutToTest';
 
 
 
+const router = createRouter('', { user: 'nikita' }, '', {
+  initialProps: {},
+  pageLoader: jest.fn(),
+  App: jest.fn(),
+  Component: jest.fn(),
 
+});
 
+// import UserInfo from './$user';
 
+const UserInfo : NextPage = () => {
+  // console.log('useRouter:', useRouter);
+  // const router = useRouter();
+  console.log('router in useRouter:', router);
 
+  const { query } = router;
+
+  return (
+    <div>
+      Hello
+      {query.user}
+    </div>
+  );
+};
+
+afterEach(cleanup);
+
+it('Should render correctly on route: /users/nikita', async () => {
+  const { getByText } = render(
+    <RouterContext.Provider value={router}>
+      {/* <LayoutToTest />/ */}
+      <UserInfo />
+    </RouterContext.Provider>,
+  );
+
+  await waitFor(() => getByText(/Hello nikita!/i));
+});
+
+// test('Layout', () => {
+//   jest.mock('next/router', () => ({ push: jest.fn() }));
+//   // jest.spyOn(hooks, 'useRouter').mockImplementation(() => (router));
+//   const { container } = render(<LayoutToTest />);
+
+//   // const { container } = render(
+//   //   <RouterContext.Provider value={router}>
+//   //     <LayoutToTest />
+//   //   </RouterContext.Provider>,
+//   // );
+
+//   expect(container).toMatchInlineSnapshot('');
+// });
 
 // test("Layout Responsive mobile", () => {
 
