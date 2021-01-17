a955c3c5a9340517d54e46d6426d301d27c91ae7
clement.dubois <clement.dubois@epitech.eu>
2021-01-17 14:26:27 +0100

    fix jest open handle

--- back/tests/__helpers__/context.ts
+++ back/tests/__helpers__/context.ts
@@ -5,17 +5,14 @@ import { DocumentNode, execute, toPromise } from 'apollo-link';
 import fetch from 'isomorphic-fetch';
 import fetchCookie from 'fetch-cookie';
 import axios, { AxiosInstance } from 'axios';
-import { SetupServerApi } from 'msw/node';
 
 import Server from '../../src/Server';
-import { setupMockServer } from '../mocks/server';
 
 interface TestServer {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   graphql: (query: DocumentNode, variables?: unknown) => Promise<any>;
   stop: () => void;
   baseURL: string;
-  mockServer: SetupServerApi,
 }
 
 export interface TestContext {
@@ -26,7 +23,6 @@ function startTestServer(): TestServer {
   const server = new Server();
   const httpServer = server.listen(0);
   const baseURL = `http://localhost:${(httpServer.address() as AddressInfo).port}`;
-  const mockServer = setupMockServer();
 
   const { port } = httpServer.address() as AddressInfo;
 
@@ -47,7 +43,6 @@ function startTestServer(): TestServer {
     stop,
     graphql,
     baseURL,
-    mockServer,
   };
 }
 
@@ -57,18 +52,13 @@ export function createTestContext(): TestContext {
   beforeAll(() => {
     const testServer = startTestServer();
 
-    ctx.server.mockServer = testServer.mockServer;
-    ctx.server.mockServer.listen();
     ctx.server.graphql = testServer.graphql;
     ctx.server.stop = testServer.stop;
     ctx.server.baseURL = testServer.baseURL;
   });
 
-  afterEach(() => ctx.server.mockServer.resetHandlers());
-
-  afterAll(async () => {
-    ctx.server.mockServer.close();
-    await ctx.server.stop();
+  afterAll(() => {
+    ctx.server.stop();
   });
 
   return ctx;
