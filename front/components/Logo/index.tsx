60ee9b996e36c3dae8e08f2a226a89f5d0c4fa4b
Itagiba ALIX <itagiba.alix@gmail.com>
2021-04-13 20:58:34 +0200

    fix: cleanup eslint error

--- front/components/Logo/index.tsx
+++ front/components/Logo/index.tsx
@@ -69,7 +69,7 @@ const Logo = ({ type = 'classic', size = null, className }: LogoProps): JSX.Elem
       <a className={classes.link}>
         <div className={classes.logo}>
           <Image
-            alt={type+"-"+size +" c3pm logo"}
+            alt={`${type}-${size} c3pm logo`}
             src={src}
             className={className}
             width={dimension.width}
