005dde846ab9e1511e062d2cb8bb318c6441b5b1
angela-boyadjian <boyadjian.angela@gmail.com>
2021-06-21 15:58:38 +0200

    tests: add cypress tests for search

--- e2e/cypress/e2e/search.js
+++ e2e/cypress/e2e/search.js
@@ -1,16 +1,25 @@
 describe('search', () => {
-  it('should redirect on result page and show result', () => {
-    const p = { name: 'hello', version: '1.0.0' };
-    cy.createUserAndPublish(p).then((user) => {
-      cy.visit('/');
-      cy.findByPlaceholderText(/search\.\.\./i).type(`${p.name}\n`);
-      cy.checkPackageCard({ ...p, author: user.username });
-    });
-  });
-
+  const p = { name: 'boost', version: '6.4.5' };
   it('should redirect on result page and show no result', () => {
     cy.visit('/');
     cy.findByPlaceholderText(/search\.\.\./i).type('bonjour\n');
     cy.findByTestId('number-of-packages').contains('0');
+  });
+  it('should redirect on result page and packages with boost name', () => {
+    cy.visit('/');
+    cy.findByPlaceholderText(/search\.\.\./i).type('boost\n');
+    cy.checkPackageCard({ ...p, author: 'demodemo' });
+  });
+  it('should redirect on result page and show all packages', () => {
+    cy.visit('/');
+    cy.findByPlaceholderText(/search\.\.\./i).type('{enter}');
+    cy.findByTestId('number-of-packages').contains('4');
+  });
+  it('should redirect on result page, click on tag and show correct package', () => {
+    cy.visit('/');
+    cy.findByPlaceholderText(/search\.\.\./i).type('{enter}');
+    cy.contains('boost').click();
+    cy.checkPackageCard({ ...p, author: 'demodemo' });
+    cy.findByTestId('number-of-packages').contains('1');
   });
 });
