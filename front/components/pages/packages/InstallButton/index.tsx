a51599a43e2688abc8a58a1963b7cf4272ca0db7
Itagiba ALIX <itagiba.alix@gmail.com>
2021-10-05 11:41:16 +0200

    feat: animation copied install button

--- front/components/pages/packages/InstallButton/index.tsx
+++ front/components/pages/packages/InstallButton/index.tsx
@@ -1,6 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Tooltip from '@material-ui/core/Tooltip';
 import React, { useState } from 'react';
+import checkCopied from 'public/assets/checkCopied.json';
+import Lottie from 'lottie-react';
 
 export interface InstallButtonProps {
   packageName: string;
@@ -12,21 +14,34 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 12,
     border: `1px solid ${theme.palette.primary.main}`,
     borderRadius: theme.shape.borderRadius,
-    padding: theme.spacing(1),
     width: 200,
     backgroundColor: 'transparent',
     outline: 'none',
     cursor: 'pointer',
     whiteSpace: 'nowrap',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
     textAlign: 'left',
+    flexDirection: 'row',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
+  container: {
+    width: '30px',
+    height: '30px',
+  },
+  textContainer: {
+    paddingLeft: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+    paddingTop: theme.spacing(1),
+    textOverflow: 'ellipsis',
+    overflow: 'hidden',
+    maxWidth: '155px',
+  }
 }));
 
 function InstallButton({ packageName }: InstallButtonProps): JSX.Element {
   const classes = useStyles();
-
+  const [isCopied, setIsCopied] = useState(false)
   const [tooltipTitle, setTooltipTitle] = useState<string>('copy');
 
   const addCmd = `ctpm add ${packageName}`;
@@ -39,13 +54,23 @@ function InstallButton({ packageName }: InstallButtonProps): JSX.Element {
     document.execCommand('copy');
     document.body.removeChild(dummy);
     setTooltipTitle('copied');
+    setIsCopied(true);
   }
 
   return (
     <Tooltip title={tooltipTitle}>
       <button className={classes.add} onClick={() => textToClipboard(addCmd)} type="button">
-        $&nbsp;
-        {addCmd}
+        <div className={classes.textContainer}>
+          $&nbsp;
+          {addCmd}
+        </div>
+        {
+          isCopied ? 
+            <div className={classes.container}>
+              <Lottie loop={false} animationData={checkCopied} />
+            </div>
+          : null
+        }
       </button>
     </Tooltip>
   );
