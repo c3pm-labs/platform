c16e1a06e0e19b200781986ddc234d9640011b35
Chloé <chloe.bourbion@gmail.com>
2021-11-02 17:38:11 +0100

    remove tags test

--- e2e/cypress/e2e/search.js
+++ e2e/cypress/e2e/search.js
@@ -4,24 +4,4 @@ describe('search', () => {
     cy.findByPlaceholderText(/browse packages\.\.\./i).type('fakepackage\n');
     cy.findByTestId('number-of-packages').contains('0');
   });
-
-  it('should filter by boost tag', () => {
-    cy.visit('/search');
-    cy.findByTestId('button-boost').click();
-    cy.checkPackageCard({ name: 'boost', version: '6.4.5', author: 'demodemo' });
-  });
-
-  it('should filter by graphics tag', () => {
-    cy.visit('/search');
-    cy.findByTestId('button-graphics').click();
-    cy.checkPackageCard({ name: 'irrlicht', version: '2.4.5', author: 'demodemo' });
-    cy.checkPackageCard({ name: 'sfml', version: '2.4.5', author: 'demodemo' });
-  });
-
-  it('should filter by graphics tag + irr name', () => {
-    cy.visit('/search');
-    cy.findByTestId('button-graphics').click();
-    cy.findByPlaceholderText(/search\.\.\./i).type('irr\n');
-    cy.checkPackageCard({ name: 'irrlicht', version: '2.4.5', author: 'demodemo' });
-  });
 });
