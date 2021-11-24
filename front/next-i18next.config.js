c0de1fc6c57138a560237c79f961195563f67a53
Gabriel Colson <gabrielcolson@hotmail.fr>
2021-11-24 17:22:42 +0100

    fix: add locale path (#115)

--- front/next-i18next.config.js
+++ front/next-i18next.config.js
@@ -1,6 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'fr',
     locales: ['fr', 'en'],
   },
+  localePath: path.resolve('./public/locales'),
 };
